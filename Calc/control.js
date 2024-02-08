f9d79d8385809bbf9437aeb969a30c83026e4aae
DF490 <91211468+DF490@users.noreply.github.com>
2024-02-08 08:24:24 +0100

    Add files via upload

--- Calc/control.js
+++ Calc/control.js
@@ -5,16 +5,37 @@ const btn = document.querySelectorAll(".botones");
 btn.forEach(boton => {
  boton.addEventListener("click", () =>{
     const pulsado = boton.textContent;
-    
-    if( pantalla2.textContent === "0"){
-        pantalla2.textContent = pulsado
-    }else{
-        pantalla2.textContent += pulsado;
-    }
 
-    if(boton.id === "clear"){
-        pantalla2.textContent = "0";
+    if(boton.id === "igual"){
+        try{
+            pantalla1.textContent = eval(pantalla2.textContent);
+        }catch{
+            pantalla1.textContent = "Error";
+            pantalla2.textContent = "0";
+        }
         return;
     }
+    
+    if(boton.id === "borrar" ){
+        if(pantalla2.textContent.length === 1  || pantalla1.textContent === "Error"){
+            pantalla2.textContent = "0";            
+        }else{
+            pantalla2.textContent = pantalla2.textContent.slice(0, -1);
+        }
+        return;
+    }
+    
+    if(pantalla2.textContent === "0"){
+        pantalla2.textContent = pulsado;                
+    }else{
+            pantalla2.textContent += pulsado;            
+        }   
+
+    if(boton.id === "clear"){        
+        pantalla1.textContent = "0";
+        pantalla2.textContent = "0";        
+        return;
+    }
+    
  })
 })
